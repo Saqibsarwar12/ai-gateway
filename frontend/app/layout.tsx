b9f2fc7e1508e5fa5e35385b28bc45007f36d753
Saqib <saqib@zocomputer.io>
2026-06-05 13:39:23 +0000

    fix: remove Google Fonts preconnect from layou

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -16,10 +16,6 @@ export const viewport: Viewport = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
-      </head>
       <body>
         <AuthProvider>{children}</AuthProvider>
       </body>
